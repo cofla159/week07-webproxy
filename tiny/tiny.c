8429da8d7e18fc5d6f819b011ab30be2456b2043
황채림 <cofla159@naver.com>
2023-04-15 12:22:37 +0000

    숙제 11.7 mp4

--- tiny/tiny.c
+++ tiny/tiny.c
@@ -192,6 +192,8 @@ void get_filetype(char *filename, char *filetype)
     strcpy(filetype, "image/gif");
   else if (strstr(filename, ".jpg"))
     strcpy(filetype, "image/jpeg");
+  else if (strstr(filename, ".mp4"))
+    strcpy(filetype, "video/mp4");
   else
     strcpy(filetype, "text/plain");
 }
